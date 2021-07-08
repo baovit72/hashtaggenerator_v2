8046cbdf95563558a5c72d2c664371546a64be18
Bao Ho Nguyen <baohn2@vng.com.vn>
2021-07-08 12:17:35 +0700

    'updated'

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const axios = require("axios");
 const utils = require("./utils");
+var WordPOS = require("wordpos"),
+  wordpos = new WordPOS();
 async function run() {
-  const browser = await utils.getPuppeteerBrowser();
-  const page = await browser.newPage();
-  async function waitThenGetElement(selector, unique) {
-    await page.waitForSelector(selector, { timeout: 60000 });
-    await utils.sleep(2000);
-    if (unique) {
-      return await page.$(selector);
-    }
-    return await page.$$(selector);
-  }
-  async function typeToInput(selector, text) {
-    await waitThenGetElement(selector);
-    await page.type(selector, text, { delay: 50 });
-  }
-  async function evalScript(selector, evalCb) {
-    await waitThenGetElement(selector);
-    return await page.$eval(selector, evalCb);
-  }
+  const tags = await utils.readCsv("tag.csv");
+  const titles = await utils.readCsv("title.csv");
 }
 
 run();
